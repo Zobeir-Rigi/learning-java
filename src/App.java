e68c7d0f56aa336f2e5e95e1421cae6f39b1020f
zobeir-rigi <rigi.zobir@yahoo.com>
2025-03-08 01:41:14 +0000

    how to input in java

--- src/App.java
+++ src/App.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!This is zobeir");
@@ -12,6 +14,11 @@ public class App {
         System.out.println(myName + " " + "is " + age +", and his name starts with "
         +initialLetter +" and End with " + lastLetter + ". The good news is,He will be working in the "
        + "Centrica "+ atBritishGas+ " as a software developer soon." );
+
+       System.out.println("Enter Your new Name haha ...");
+       Scanner input = new Scanner(System.in);
+         myName  =input.next();
+         System.out.println("Your new name is " + myName);
 
     }
 }
