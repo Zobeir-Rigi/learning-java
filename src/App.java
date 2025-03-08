c3b345bbf184e04910bbf0574e1ed6f9e52f2e21
zobeir-rigi <rigi.zobir@yahoo.com>
2025-03-08 01:29:20 +0000

    practicing data types

--- src/App.java
+++ src/App.java
@@ -1,5 +1,17 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!This is zobeir");
+
+        String myName = "Zobeir";
+        int age = 32;
+        double gpa = 4.0;
+        char initialLetter = myName.charAt(0);
+        char lastLetter = myName.charAt(5);
+        boolean atBritishGas = true;
+
+        System.out.println(myName + " " + "is " + age +", and his name starts with "
+        +initialLetter +" and End with " + lastLetter + ". The good news is,He will be working in the "
+       + "Centrica "+ atBritishGas+ " as a software developer soon." );
+
     }
 }
